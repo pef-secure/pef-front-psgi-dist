458ec8c86db47e8aef9d3ee49a59ea29f3048b87
pef-secure <pef-secure@yandex.ru>
2014-10-16 18:01:07 +0200

    [L]ast rule can have status with empty headers and body

--- lib/PEF/Front/Route.pm
+++ lib/PEF/Front/Route.pm
@@ -2,6 +2,7 @@ package PEF::Front::Route;
 
 use strict;
 use warnings;
+use Scalar::Util qw(blessed);
 use PEF::Front::Config;
 use PEF::Front::Request;
 use PEF::Front::Response;
@@ -17,43 +18,43 @@ my $tranpos = 3;
 sub add_route {
 	my @params = @_;
 	shift @params if @params & 1;
-	for (my $i = 0; $i < @params; $i += 2) {
+	for (my $i = 0 ; $i < @params ; $i += 2) {
 		my ($rule, $rdest) = @params[$i, $i + 1];
 		push @rewrite, [$rule, undef, {}, undef];
 		my $ri = $#rewrite;
-		if (ref($rdest) eq 'ARRAY') {
+		if (ref ($rdest) eq 'ARRAY') {
 			$rewrite[$ri][$nurlpos] = $rdest->[0];
 			my @flags = split /[, ]+/, $rdest->[1] if @$rdest > 1;
 			for my $f (@flags) {
 				my ($p, $v) = split /=/, $f, 2;
 				$rewrite[$ri][$flagpos]{uc $p} = $v;
 			}
-		} elsif (ref($rdest) && ref($rdest) ne 'CODE') {
+		} elsif (ref ($rdest) && ref ($rdest) ne 'CODE') {
 			die "bad routing rule at $rule";
 		} else {
 			$rewrite[$ri][$nurlpos] = $rdest;
 		}
-		if (ref($rule) eq 'Regexp') {
-			if (!ref($rewrite[$ri][$nurlpos])) {
+		if (ref ($rule) eq 'Regexp') {
+			if (!ref ($rewrite[$ri][$nurlpos])) {
 				$rewrite[$ri][$tranpos] =
 				    eval "sub {my \$request = \$_[0]; my \$url = \$request->path; return \$url if \$url =~ s\"$rule\""
 				  . $rewrite[$ri][$nurlpos] . "\""
-				  . (exists($rewrite[$ri][$flagpos]{RE}) ? $rewrite[$ri][$flagpos]{RE} : "")
+				  . (exists ($rewrite[$ri][$flagpos]{RE}) ? $rewrite[$ri][$flagpos]{RE} : "")
 				  . "; return }";
 			} else {
 				$rewrite[$ri][$tranpos] =
 				    eval "sub {my \$request = \$_[0]; "
 				  . "my \@params = \$request->path =~ "
 				  . (
-					exists($rewrite[$ri][$flagpos]{RE})
+					exists ($rewrite[$ri][$flagpos]{RE})
 					? "m\"$rule\"" . $rewrite[$ri][$flagpos]{RE} . ";"
 					: "m\"$rule\"; "
 				  )
 				  . "return \$rewrite[$ri][$nurlpos]->(\$request, \@params) if \@params;"
 				  . "return; }";
 			}
-		} elsif (ref($rule) eq 'CODE') {
-			if (not defined($rewrite[$ri][$nurlpos])) {
+		} elsif (ref ($rule) eq 'CODE') {
+			if (not defined ($rewrite[$ri][$nurlpos])) {
 				$rewrite[$ri][$tranpos] = $rule;
 			} elsif (!ref $rewrite[$ri][$nurlpos]) {
 				$rewrite[$ri][$tranpos] =
@@ -92,7 +93,7 @@ sub import {
 sub rewrite {
 	my $request = $_[0];
 	my $env     = $request->env;
-	for (my $i = 0; $i < @rewrite; ++$i) {
+	for (my $i = 0 ; $i < @rewrite ; ++$i) {
 		my $rewrite_func  = $rewrite[$i][$tranpos];
 		my $rewrite_flags = $rewrite[$i][$flagpos];
 		if ((my $npi = $rewrite_func->($request))) {
@@ -107,8 +108,17 @@ sub rewrite {
 				$http_response ||= PEF::Front::Response->new();
 				$http_response->redirect($npi, $rewrite_flags->{R});
 			}
-			return $http_response if $http_response;
-			$npi = '/' . $npi if !$npi || substr($npi, 0, 1) ne '/';
+			if (   !$http_response
+				&& exists ($rewrite_flags->{L})
+				&& defined ($rewrite_flags->{L})
+				&& $rewrite_flags->{L} > 0)
+			{
+				$http_response = PEF::Front::Response->new();
+				$http_response->status($rewrite_flags->{L});
+			}
+			return $http_response
+			  if $http_response && blessed($http_response) && $http_response->isa('PEF::Front::Response');
+			$npi = '/' . $npi if !$npi || substr ($npi, 0, 1) ne '/';
 			$env->{PATH_INFO} = $npi;
 			last if %$rewrite_flags and exists $rewrite_flags->{L};
 		}
@@ -121,7 +131,7 @@ sub to_app {
 		my $request  = PEF::Front::Request->new($_[0]);
 		my $response = rewrite($request);
 		return $response->response() if $response;
-		if (url_contains_lang && (substr($request->path, 0, 1) ne '/' || substr($request->path, 0, 3) ne '/')) {
+		if (url_contains_lang && (substr ($request->path, 0, 1) ne '/' || substr ($request->path, 0, 3) ne '/')) {
 			my $lang = guess_lang($request);
 			if ($request->method eq 'GET') {
 				my $http_response = PEF::Front::Response->new();
@@ -130,16 +140,16 @@ sub to_app {
 			} else {
 				my $env = $request->env;
 				my $path = $env->{PATH_INFO} || '/';
-				$path = '/' . $path if substr($path, 0, 1) ne '/';
+				$path = '/' . $path if substr ($path, 0, 1) ne '/';
 				$env->{PATH_INFO} = "/$lang$path";
 			}
 		}
 		my $lang_offset = (url_contains_lang) ? 3 : 0;
-		if (substr($request->path, $lang_offset, 4) eq '/app') {
+		if (substr ($request->path, $lang_offset, 4) eq '/app') {
 			return PEF::Front::TemplateHtml::handler($request);
-		} elsif (substr($request->path, $lang_offset, 5) eq '/ajax'
-			|| substr($request->path, $lang_offset, 7) eq '/submit'
-			|| substr($request->path, $lang_offset, 4) eq '/get')
+		} elsif (substr ($request->path, $lang_offset, 5) eq '/ajax'
+			|| substr ($request->path, $lang_offset, 7) eq '/submit'
+			|| substr ($request->path, $lang_offset, 4) eq '/get')
 		{
 			return PEF::Front::Ajax::handler($request);
 		} else {
