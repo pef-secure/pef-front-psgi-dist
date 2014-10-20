2c492ca3ab969dc8964c8e83474f745c36ae362a
pef-secure <pef-secure@yandex.ru>
2014-10-20 16:17:12 +0200

    вынесение общего с TemplateHtml кода исправление utf-8 в шаблонных обработках из описания модели установка пришедших из модели заголовков и кук удаление обработки секции result описания модели, теперь это делается скомпилированными в Validator функциями

--- lib/PEF/Front/Ajax.pm
+++ lib/PEF/Front/Ajax.pm
@@ -7,37 +7,36 @@ use JSON;
 use URI::Escape;
 use Template::Alloy;
 use Data::Dumper;
+use Scalar::Util qw(blessed);
 use PEF::Front::Config;
 use PEF::Front::Cache;
 use PEF::Front::Validator;
 use PEF::Front::NLS;
 use PEF::Front::Response;
 
-sub ajax {
-	my $request       = $_[0];
-	my $form          = $request->params;
-	my $cookies       = $request->cookies;
-	my $log           = $request->logger;
-	my $http_response = PEF::Front::Response->new();
+sub prepare_defaults {
+	my $request = $_[0];
+	my $form    = $request->params;
+	my $cookies = $request->cookies;
 	my $lang;
 	my ($src, $method, $params);
-
 	if (url_contains_lang) {
-		($lang, $src, $method, $params) = $request->path =~ m{^/([\w][\w])/(ajax|submit|get)([^/]+)/?(.*)$};
+		($lang, $src, $method, $params) = $request->path =~ m{^/([\w][\w])/(app|ajax|submit|get)([^/]+)/?(.*)$};
 		if (not defined $lang) {
+			my $http_response = PEF::Front::Response->new();
 			$http_response->redirect(301, location_error);
-			return $http_response->response();
+			return $http_response;
 		}
 	} else {
-		($src, $method, $params) = $request->path =~ m{^/(ajax|submit|get)([^/]+)/?(.*)$};
+		($src, $method, $params) = $request->path =~ m{^/(app|ajax|submit|get)([^/]+)/?(.*)$};
 		if (not defined $method) {
+			my $http_response = PEF::Front::Response->new();
 			$http_response->redirect(301, location_error);
-			return $http_response->response();
+			return $http_response;
 		}
 		$lang = guess_lang($request);
 	}
-	$http_response->set_cookie(lang => $lang);
-	if ($src eq 'get' && defined ($params)) {
+	if (($src eq 'get' || $src eq 'app') && defined ($params)) {
 		$src = 'submit';
 		my @params = split /\//, $params;
 		for my $pv (@params) {
@@ -60,9 +59,7 @@ sub ajax {
 	my $ucMethod = $method;
 	$method =~ s/[[:lower:]]\K([[:upper:]])/ \l$1/g;
 	$method = lcfirst $method;
-	my %request = %$form;
-	$request{method} = $method;
-	my $defaults = {
+	return {
 		ip        => $request->remote_ip,
 		lang      => $lang,
 		hostname  => $request->hostname,
@@ -71,7 +68,26 @@ sub ajax {
 		headers   => $request->headers,
 		scheme    => $request->scheme,
 		cookies   => $cookies,
+		method    => $method,
+		src       => $src,
 	};
+}
+
+sub ajax {
+	my $request  = $_[0];
+	my $form     = $request->params;
+	my $cookies  = $request->cookies;
+	my $log      = $request->logger;
+	my $defaults = prepare_defaults($request);
+	if (blessed($defaults) && $defaults->isa('PEF::Front::Response')) {
+		return $defaults->response();
+	}
+	my $http_response = PEF::Front::Response->new();
+	my $lang          = $defaults->{lang};
+	my %request       = %$form;
+	my $src           = $defaults->{src};
+	$request{method} = $defaults->{method};
+	$http_response->set_cookie(lang => $lang);
 	my $vreq = eval { validate(\%request, $defaults) };
 	my $response;
 	my $json = $src eq 'ajax';
@@ -135,160 +151,44 @@ sub ajax {
 				V2EQUALS    => 0,
 				ENCODING    => "UTF-8"
 			);
-
-			# this bug was fixed in Template::Alloy 1.018
-			# $stash = JSON->new->decode(JSON->new->utf8->encode($stash));
 			$stash->{uri_unescape} = sub { uri_unescape @_ };
-			my $eval_result = sub {
-				if (substr ($_[0], 0, 3) eq 'TT ') {
-					my $url = substr ($_[0], 3);
-					my $tmpl = encode_utf8 '[% url(' . $url . ') %]';
-					$tt->process_simple(\$tmpl, $stash, \$new_loc) or do {
-						$log->({
-								level   => "debug",
-								message => "error processing resulting url ($vreq->{method}) '$_[0]': " . $tt->error()
-							}
-						);
-						$log->({level => "debug", message => "error: " . Dumper($vreq, $response)});
-						$new_loc = location_error;
-						return;
-					};
-				} else {
-					$new_loc = $_[0];
-				}
-				$new_loc =~ s/^\s*//;
-				$new_loc =~ s/\s*$//;
-				$log->({level => "debug", message => "new location: $new_loc"});
-				return $new_loc;
-			};
-			if (ref ($result)) {
-				my $found      = 0;
-				my $find_redir = sub {
-					return if $json;
-					my $arr = $_[0];
-					$log->({level => "debug", message => "looking for redirect: " . Dumper($arr)});
-					$arr = [$_[0]] if !ref ($arr);
-					for my $ru (@$arr) {
-						if ($eval_result->($ru)) {
-							$found = 1;
-							$log->error("found redirect: $ru");
-							last;
-						}
-					}
-				};
-				my $test_sub = sub {
-					my $rc = $_[0];
-					$log->({level => "debug", message => "testing return code $rc"});
-					if (exists ($result->{$rc})) {
-						if (ref ($result->{$rc})) {
-							if (ref ($result->{$rc}) eq 'ARRAY') {
-								$find_redir->($result->{$rc});
-							} else {
-								$find_redir->($result->{$rc}{redirect}) if exists ($result->{$rc}{redirect});
-								if (exists $result->{$rc}{filter}) {
-									my $class = substr ($result->{$rc}{filter}, 0, rindex ($result->{$rc}{filter}, "::"));
-									my $func = substr ($result->{$rc}{filter}, rindex ($result->{$rc}{filter}, "::") + 2);
-									(my $clf = $class) =~ s|::|/|g;
-									my $mrf             = out_filter_dir . "/$clf.pm";
-									my $filter_response = eval {
-										no strict 'refs';
-										require $mrf;
-										my $fr = app_namespace . $class . "::$func";
-										return {result => 'INTERR', answer => 'Bad output filter', answer_args => []}
-										  if not defined &{$fr};
-										return $fr->($response, $defaults);
-									};
-								}
-								if (exists $result->{$rc}{answer}) {
-									if (substr ($result->{$rc}{answer}, 0, 3) eq 'TT ') {
-										my $exp = substr ($result->{$rc}{'answer'}, 3);
-										my $tmpl = encode_utf8 '[% ' . $exp . ' %]';
-										my $out;
-										$response->{answer} = $out if $tt->process_simple(\$tmpl, $stash, \$out);
-									}
-								}
-								if (exists $result->{$rc}{'set-cookie'}) {
-									my $cl = $result->{$rc}{'set-cookie'};
-									for my $cn (keys %$cl) {
-										my $cv = $cl->{$cn};
-										my ($value);
-										my %other_params;
-										if (ref ($cv)) {
-											if (exists $cv->{value}) {
-												$value = $cv->{value};
-											} else {
-												$log->({
-														level   => "debug",
-														message => "error processing set cookie $cn: undefined value"
-													}
-												);
-												$value = '';
-											}
-											for my $pn (qw/expires domain path secure max-age httponly/) {
-												if (exists $cv->{$pn}) {
-													$other_params{$pn} = $cv->{$pn};
-												}
-											}
-										} else {
-											$value = $cv;
-										}
-										$other_params{path} = "/" if not exists $other_params{path};
-										if (substr ($value, 0, 3) eq 'TT ') {
-											my $exp = substr ($value, 3);
-											my $tmpl = encode_utf8 '[% ' . $exp . ' %]';
-											my $out;
-											$tt->process(\$tmpl, $stash, \$out) and do {
-												$http_response->set_cookie(
-													$cn, {
-														value => $out,
-														%other_params
-													}
-												);
-												1;
-											  }
-											  or do {
-												$log->({
-														level   => "debug",
-														message => "error processing set cookie $cn: " . $tt->error() . ": " . Dumper($tmpl, $stash)
-													}
-												);
-											  };
-										} else {
-											$http_response->set_cookie(
-												$cn, {
-													value => $value,
-													%other_params
-												}
-											);
-
-										}
-									}
-								}
-								if (exists $result->{$rc}{'unset-cookie'}) {
-									$http_response->set_cookie(
-										$result->{$rc}{'unset-cookie'}, {
-											value   => '',
-											expires => -3600
-										}
-									);
-								}
-							}
-						} else {
-							$find_redir->($result->{$rc});
-						}
-					}
-				};
-				$test_sub->($response->{result});
-				if (!$found && exists ($result->{DEFAULT})) {
-					$test_sub->('DEFAULT');
-				}
-			} else {
-				$eval_result->($result);
-			}
+			my $err;
+			($new_loc, $response) =
+			  get_method_attrs($vreq => 'result_sub')->($response, $defaults, $stash, $http_response, $tt);
 		}
 	} else {
 		$log->({level => "debug", message => "validate error: " . Dumper($@, \%request)});
 		$response = (ref ($@) eq 'HASH' ? $@ : {result => 'INTERR', answer => 'Internal Error', answer_args => []});
+	}
+	if (exists $response->{answer_headers} and 'ARRAY' eq ref $response->{answer_headers}) {
+		while (@{$response->{answer_headers}}) {
+			if (ref ($response->{answer_headers}[0])) {
+				if (ref ($response->{answer_headers}[0]) eq 'HASH') {
+					$http_response->add_header(%{$response->{answer_headers}[0]});
+				} else {
+					$http_response->add_header(@{$response->{answer_headers}[0]});
+				}
+				shift @{$response->{answer_headers}};
+			} else {
+				$http_response->add_header($response->{answer_headers}[0], $response->{answer_headers}[1]);
+				splice @{$response->{answer_headers}}, 0, 2;
+			}
+		}
+	}
+	if (exists $response->{answer_cookies} and 'ARRAY' eq ref $response->{answer_cookies}) {
+		while (@{$response->{answer_cookies}}) {
+			if (ref ($response->{answer_cookies}[0])) {
+				if (ref ($response->{answer_cookies}[0]) eq 'HASH') {
+					$http_response->set_cookie(%{$response->{answer_cookies}[0]});
+				} else {
+					$http_response->set_cookie(@{$response->{answer_cookies}[0]});
+				}
+				shift @{$response->{answer_cookies}};
+			} else {
+				$http_response->set_cookie($response->{answer_headers}[0], $response->{answer_headers}[1]);
+				splice @{$response->{answer_cookies}}, 0, 2;
+			}
+		}
 	}
   out:
 	if ($json) {
@@ -301,36 +201,6 @@ sub ajax {
 		$http_response->set_body(encode_json($response));
 		return $http_response->response();
 	} else {
-		if (exists $response->{answer_headers} and 'ARRAY' eq ref $response->{answer_headers}) {
-			while (@{$response->{answer_headers}}) {
-				if (ref ($response->{answer_headers}[0])) {
-					if (ref ($response->{answer_headers}[0]) eq 'HASH') {
-						$http_response->add_header(%{$response->{answer_headers}[0]});
-					} else {
-						$http_response->add_header(@{$response->{answer_headers}[0]});
-					}
-					shift @{$response->{answer_headers}};
-				} else {
-					$http_response->add_header($response->{answer_headers}[0], $response->{answer_headers}[1]);
-					splice @{$response->{answer_headers}}, 0, 2;
-				}
-			}
-		}
-		if (exists $response->{answer_cookies} and 'ARRAY' eq ref $response->{answer_cookies}) {
-			while (@{$response->{answer_cookies}}) {
-				if (ref ($response->{answer_cookies}[0])) {
-					if (ref ($response->{answer_cookies}[0]) eq 'HASH') {
-						$http_response->set_cookie(%{$response->{answer_cookies}[0]});
-					} else {
-						$http_response->set_cookie(@{$response->{answer_cookies}[0]});
-					}
-					shift @{$response->{answer_cookies}};
-				} else {
-					$http_response->set_cookie($response->{answer_headers}[0], $response->{answer_headers}[1]);
-					splice @{$response->{answer_cookies}}, 0, 2;
-				}
-			}
-		}
 		if (exists $response->{answer_status} and $response->{answer_status} > 100) {
 			$http_response->status($response->{answer_status});
 			if (   $response->{answer_status} > 300
