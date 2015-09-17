2f4e43b3cb5158b3c542e955c551bdc249a283a9
pef-secure <pef-secure@yandex.ru>
2015-09-17 14:56:16 +0200

    make captcha initialization checks lazy

--- lib/PEF/Front/Captcha.pm
+++ lib/PEF/Front/Captcha.pm
@@ -14,25 +14,25 @@ use Fcntl qw(:DEFAULT :flock);
 our $cfg_captcha_db;
 our $cfg_www_static_captchas_dir;
 our $cfg_captcha_image_class;
+our $initialized = 0;
 
-BEGIN {
-	if (grep { /AppFrontConfig\.pm$/ } keys %INC) {
-		$cfg_captcha_db              = cfg_captcha_db();
-		$cfg_www_static_captchas_dir = cfg_www_static_captchas_dir();
-		$cfg_captcha_image_class     = cfg_captcha_image_class();
-		no strict 'refs';
-		for ($cfg_captcha_db, $cfg_www_static_captchas_dir) {
-			die "$_ must be directory "         unless -d $_;
-			die "directory $_ must be writable" unless -w $_;
-			$_ .= "/" unless substr ($_, -1, 1) eq '/';
-		}
-		$cfg_captcha_image_class = "PEF::Front::SecureCaptcha"
-		  if not $cfg_captcha_image_class
-		  or not %{$cfg_captcha_image_class . "::"}
-		  or not $cfg_captcha_image_class->can("generate_image");
-		eval "use $cfg_captcha_image_class";
-		croak "error loading captcha image class $cfg_captcha_image_class: $@" if $@;
+sub initialize {
+	$cfg_captcha_db              = cfg_captcha_db();
+	$cfg_www_static_captchas_dir = cfg_www_static_captchas_dir();
+	$cfg_captcha_image_class     = cfg_captcha_image_class();
+	no strict 'refs';
+	for ($cfg_captcha_db, $cfg_www_static_captchas_dir) {
+		die "$_ must be directory "         unless -d $_;
+		die "directory $_ must be writable" unless -w $_;
+		$_ .= "/" unless substr ($_, -1, 1) eq '/';
 	}
+	$cfg_captcha_image_class = "PEF::Front::SecureCaptcha"
+	  if not $cfg_captcha_image_class
+	  or not %{$cfg_captcha_image_class . "::"}
+	  or not $cfg_captcha_image_class->can("generate_image");
+	eval "use $cfg_captcha_image_class";
+	croak "error loading captcha image class $cfg_captcha_image_class: $@" if $@;
+	$initialized = 1;
 }
 
 sub _random {
@@ -61,6 +61,7 @@ sub generate_code {
 
 sub make_captcha {
 	my $req = $_[0];
+	initialize if not $initialized;
 	my ($str, $sha1) = generate_code($req->{size}, cfg_captcha_expire_sec());
 	my $image_init = cfg_captcha_image_init();
 	$image_init = {} if not $image_init or 'HASH' ne ref $image_init;
